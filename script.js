c0ecf46eaa5fca7cd382cc99cc97a4926e170065
francisco-costa98 <franciscomffcosta@hotmail.com>
2020-09-22 09:21:53 +0100

    added css and basic button binding

---
+++ script.js
@@ -0,0 +1,9 @@
+let time = 0;
+let start_button = document.getElementById('start_button');
+let stop_button = document.getElementById('stop_button');
+
+start_button.addEventListener('click', start_timer);
+
+function start_timer(){
+    console.log('hello')
+}
